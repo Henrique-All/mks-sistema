850aed798ccb1dcc61555f05f07d86f5f6d32288
CH <carlinhos.biocraft@gmail.com>
2023-11-09 20:07:06 -0300

    Feat: create card drawer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { HeaderComponent } from "@/components/HeaderComponent";
 import GlobalStyles from "@/styles/GlobalStyles";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <HeaderComponent />
-
       <GlobalStyles />
     </>
   );
