e00df2a7b685661a438a9bca8334ea4e968cb5c7
CH <carlinhos.biocraft@gmail.com>
2023-11-11 08:59:31 -0300

    fix: Next/font

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require("next/jest");
+
+// Providing the path to your Next.js app which will enable loading next.config.js and .env files
+const createJestConfig = nextJest({ dir: "./" });
+
+// Any custom config you want to pass to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@/src/components/(.*)$": "<rootDir>/src/components/$1",
+    "^@/src/pages/(.*)$": "<rootDir>/src/pages/$1",
+  },
+  testEnvironment: "jest-environment-jsdom",
+};
+
+// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
+module.exports = createJestConfig(customJestConfig);
