6c24f270f3bff8b5cd8ca701935d385c68819f00
CH <carlinhos.biocraft@gmail.com>
2023-11-12 01:02:29 -0300

    fix: span 0

--- src/app/pages/home/index.tsx
+++ src/app/pages/home/index.tsx
@@ -34,7 +34,6 @@ export default function HomePage() {
 
           <Button onClick={handleButtonClick}>
             <Image src={Car} alt="" />
-            <span>0</span>
           </Button>
         </NavMain>
       </HeaderMain>
